19dc9582b23ec6ec63b5185982bfd1e4f1d9888e
302A-12\cre <a>
2024-08-27 16:09:47 +0900

    a

--- works/work2/q6.php
+++ works/work2/q6.php
@@ -2,7 +2,8 @@
 
 // 以下の配列をの要素のうち文字数が偶数のもののみ表示するループを書いてください
 $fruits = array("Apple", "Banana", "Cherry");
-for ($i = 0; $i < count($fruits); $i++)
-    if ($i % 2 == 0) {
+for ($i = 0; $i < count($fruits); $i++) {
+    if (strlen($fruits[$i]) % 2 == 0) {
         echo "$fruits[$i]\n";
     }
+}
--- works/work3/q1.php
+++ works/work3/q1.php
@@ -2,8 +2,10 @@
 
 // 引数として受け取った数値を2倍にして返す関数を完成させなさい。
 
-double();
+$a = double(8);
+echo $a;
 
-function double()
+function double($double)
 {
+    return $double * 2;
 }
